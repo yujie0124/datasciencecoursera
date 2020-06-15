d37c54458d5bc03ec24bebd44ffdc58c006ec1d8
yujie0124 <yujielim08@gmail.com>
2020-06-16 00:33:29 +0800

    Function Swirl

---
+++ function.R
@@ -0,0 +1,38 @@
+add2 <- function(x,y){
+  x+y
+}
+
+above10<- function(x){
+  use<-x>10
+  x[use]
+}
+
+#above <- function(x,n){
+ # use <- x>n
+  #x[use]
+#}
+
+#initialise n if user not enter n number
+above <- function(x,n = 10){
+  use <- x>n
+  x[use]
+}
+
+columnmean <- function(y){
+  nc <- ncol(y)
+  means <- numeric(nc)
+  for(i in 1:nc){
+    means[i]<-mean(y[,i])
+  }
+  means
+}
+
+#removing NA value
+columnmean2 <- function(y,removeNA=TRUE){
+  nc <- ncol(y)
+  means <- numeric(nc)
+  for(i in 1:nc){
+    means[i]<-mean(y[,i],na.rm = removeNA)
+  }
+  means
+}
