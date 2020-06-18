69d800077a2c5f8b1fec27c866f70e3e05371fba
yujie0124 <yujielim08@gmail.com>
2020-06-18 12:29:41 +0800

    Programming Assignment 2 complete()

--- Course 2 Assignment/pollutantmean.R
+++ Course 2 Assignment/pollutantmean.R
@@ -1,5 +1,7 @@
 pollutantmean <- function(directory, pollutant, id = 1:332) {
     names <- list.files(directory)[id]
-    read <- lapply(paste(directory,"/",names, sep = ""),read.csv)     
+    print(names)
+    read <- lapply(paste(directory,"/",names, sep = ""),read.csv)
+    print(read)
     return(mean(unlist(lapply(read, function(x){x[,pollutant]})), na.rm=T))
 }
