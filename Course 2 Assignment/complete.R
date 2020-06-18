dc8c3970131b7d8c01512dbfbc4aafb086c4f2aa
yujie0124 <yujielim08@gmail.com>
2020-06-18 12:30:51 +0800

    -

--- Course 2 Assignment/complete.R
+++ Course 2 Assignment/complete.R
@@ -1,6 +1,5 @@
 complete<-function(directory, id =1:332){
   filelist <-list.files(path = directory,pattern = ".csv",full.names = TRUE)
-  print(filelist)
   nobs<-numeric()
   for(i in id){
     data<-read.csv(filelist[i])
--- Course 2 Assignment/pollutantmean.R
+++ Course 2 Assignment/pollutantmean.R
@@ -1,7 +1,5 @@
 pollutantmean <- function(directory, pollutant, id = 1:332) {
     names <- list.files(directory)[id]
-    print(names)
     read <- lapply(paste(directory,"/",names, sep = ""),read.csv)
-    print(read)
     return(mean(unlist(lapply(read, function(x){x[,pollutant]})), na.rm=T))
 }
